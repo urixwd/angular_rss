41be997cd6cb060b91b7d3f0e0a722533c213af1
urixwd <urixwd@gmail.com>
2015-01-22 22:34:33 +0200

    Update additional.js

--- js/shared/additional.js
+++ js/shared/additional.js
@@ -1,7 +1,3 @@
-String.prototype.isEmail = function () {
-    validmailregex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9][-a-z0-9]*(\.[-a-z0-9]+)*\.([a-z][a-z]+)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-    return validmailregex.test(this);
-};
 String.prototype.isURL = function () {
     rx = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i;
     return rx.test(this);
